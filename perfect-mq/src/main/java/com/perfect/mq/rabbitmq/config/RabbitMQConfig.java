c02d16f542e2a2a4287fe3ee89ab5270fe1d5510
zxhbloomer <zxhbloomer@gmail.com>
2019-10-12 14:58:01 +0800

    ..

---
+++ perfect-mq/src/main/java/com/perfect/mq/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package com.perfect.mq.rabbitmq.config;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author：
+ * @Description: 当生产者 使用的Jackson2JsonMessageConverter 序列化 必须配置该类 消费者才可以反序列化
+ * @Date：Created
+ */
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+    @Bean public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+}
