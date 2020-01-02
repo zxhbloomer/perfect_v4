b3ca4beb22f08e9e7461cba3107e0bf661e2700e
zxhbloomer@gmail.com <Lyyzxh030324>
2020-01-02 17:15:09 +0800

    ..

--- perfect-common/src/main/java/com/perfect/common/annotation/OperationDetailLog.java
+++ perfect-common/src/main/java/com/perfect/common/annotation/OperationDetailLog.java
@@ -25,16 +25,5 @@ public @interface OperationDetailLog {
 	/** 需要记录的字段 */
 	String[] cloums() default {};
 	/** id字段的值 */
-	String id() default "";
-
-//	/** 业务名 */
-//	String name();
-//	/** 表名 */
-//	String table();
-//	/** id 在函数的字段名 */
-//	int idRef() default -1;
-//	/** 需要记录的字段 */
-//	String[] cloum() default {};
-//	/** 操作类型 */
-//	OperationEnum type();
+	String id();
 }
