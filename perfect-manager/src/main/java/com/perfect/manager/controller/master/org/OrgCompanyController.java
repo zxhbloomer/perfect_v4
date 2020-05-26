0781b4c3c35cb477681cd3febf3f08f42564ba7e
zxhbloomer@gmail.com <Lyyzxh030324>
2020-05-26 15:23:41 +0800

    ..

--- perfect-manager/src/main/java/com/perfect/manager/controller/master/org/OrgCompanyController.java
+++ perfect-manager/src/main/java/com/perfect/manager/controller/master/org/OrgCompanyController.java
@@ -29,7 +29,7 @@ import java.util.List;
  * @author zhangxh
  */
 @RestController
-@RequestMapping(value = "/api/v1/org")
+@RequestMapping(value = "/api/v1/org/company")
 @Slf4j
 @Api("公司表相关")
 public class OrgCompanyController extends BaseController {
@@ -42,7 +42,7 @@ public class OrgCompanyController extends BaseController {
 
     @SysLogAnnotion("根据查询条件，获取公司主表信息")
     @ApiOperation("根据参数id，获取公司主表信息")
-    @PostMapping("/company/list")
+    @PostMapping("/list")
     @ResponseBody
     public ResponseEntity<JsonResult<IPage<MCompanyVo>>> list(@RequestBody(required = false) MCompanyVo searchCondition){
         IPage<MCompanyVo> entity = service.selectPage(searchCondition);
@@ -51,7 +51,7 @@ public class OrgCompanyController extends BaseController {
 
     @SysLogAnnotion("公司主表数据更新保存")
     @ApiOperation("根据参数id，获取公司主表信息")
-    @PostMapping("/company/save")
+    @PostMapping("/save")
     @ResponseBody
     public ResponseEntity<JsonResult<MCompanyVo>> save(@RequestBody(required = false) MCompanyEntity bean) {
 
@@ -64,7 +64,7 @@ public class OrgCompanyController extends BaseController {
 
     @SysLogAnnotion("公司主表数据新增保存")
     @ApiOperation("根据参数id，获取公司主表信息")
-    @PostMapping("/company/insert")
+    @PostMapping("/insert")
     @ResponseBody
     public ResponseEntity<JsonResult<MCompanyVo>> insert(@RequestBody(required = false) MCompanyEntity bean) {
         if(service.insert(bean).isSuccess()){
@@ -76,7 +76,7 @@ public class OrgCompanyController extends BaseController {
 
     @SysLogAnnotion("公司主表数据导出")
     @ApiOperation("根据选择的数据，公司主表数据导出")
-    @PostMapping("/company/export_all")
+    @PostMapping("/export_all")
     public void exportAll(@RequestBody(required = false) MCompanyVo searchCondition, HttpServletResponse response) throws IOException {
         List<MCompanyVo> searchResult = service.select(searchCondition);
         List<MCompanyExportVo> rtnList = BeanUtilsSupport.copyProperties(searchResult, MCompanyExportVo.class);
@@ -86,7 +86,7 @@ public class OrgCompanyController extends BaseController {
 
     @SysLogAnnotion("公司主表数据导出")
     @ApiOperation("根据选择的数据，公司主表数据导出")
-    @PostMapping("/company/export_selection")
+    @PostMapping("/export_selection")
     public void exportSelection(@RequestBody(required = false) List<MCompanyVo> searchConditionList, HttpServletResponse response) throws IOException {
         List<MCompanyVo> searchResult = service.selectIdsInForExport(searchConditionList);
         List<MCompanyExportVo> rtnList = BeanUtilsSupport.copyProperties(searchResult, MCompanyExportVo.class);
@@ -96,7 +96,7 @@ public class OrgCompanyController extends BaseController {
 
     @SysLogAnnotion("公司主表数据逻辑删除复原")
     @ApiOperation("根据参数id，逻辑删除复原数据")
-    @PostMapping("/company/delete")
+    @PostMapping("/delete")
     @ResponseBody
     public ResponseEntity<JsonResult<String>> delete(@RequestBody(required = false) List<MCompanyVo> searchConditionList) {
         service.deleteByIdsIn(searchConditionList);
